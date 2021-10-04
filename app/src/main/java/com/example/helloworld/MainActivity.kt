85bdedf24b30a68b8ff49c5fceb95d17735b28bc
amandabriena <amanda.briena@gmail.com>
2021-10-04 12:49:45 -0300

    ajustes e formatações

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,22 +3,36 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.helloworld.Model.Dice
 
 class MainActivity : AppCompatActivity() {
+    /*
+    * Encontrando o botão  a textView que estão no layout
+    * */
+    val rollButton: Button = findViewById(R.id.button)
+    val textResult: TextView = findViewById(R.id.numDice)
+    //Criando nova instância da classe Dice
+    val dice = Dice(6, "blue")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener{
-            Toast.makeText(this, "DiceRolled", Toast.LENGTH_SHORT).show()
+        /*
+        * Botão executando o método de rolar o dado ao ser clicado
+        * */
+        rollButton.setOnClickListener {
+            rollDice()
         }
 
-        val myFirstDice = Dice(6, "blue")
-        myFirstDice.roll();
-
+    }
+    /*
+    * Método para "rolar" o dado e mostrar o resultado em tela:
+    * */
+    private fun rollDice() {
+        val diceRoll = dice.roll()
+        textResult.text = diceRoll.toString()
     }
 }
