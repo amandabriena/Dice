a7f54db299978b535c444cbc00099774de9a5d9f
amandabriena <amanda.briena@gmail.com>
2021-10-04 23:16:13 -0300

    adição de imagem do dado

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.helloworld.Model.Dice
@@ -11,8 +12,9 @@ class MainActivity : AppCompatActivity() {
     /*
     * Encontrando o botão  a textView que estão no layout
     * */
-    val rollButton: Button = findViewById(R.id.button)
-    val textResult: TextView = findViewById(R.id.numDice)
+
+
+
     //Criando nova instância da classe Dice
     val dice = Dice(6, "blue")
 
@@ -20,12 +22,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val rollButton: Button = findViewById(R.id.rollButton);
+
         /*
         * Botão executando o método de rolar o dado ao ser clicado
         * */
         rollButton.setOnClickListener {
             rollDice()
         }
+        // Inicializando o dado ao iniciar o app:
+        rollDice()
 
     }
     /*
@@ -33,6 +39,15 @@ class MainActivity : AppCompatActivity() {
     * */
     private fun rollDice() {
         val diceRoll = dice.roll()
-        textResult.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageDice)
+        val drawableResouse = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResouse)
     }
 }
